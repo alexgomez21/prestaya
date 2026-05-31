301d2acce5f084217397865422cce5a7661a5331
Alex Gomez <wagr94@gmail.com>
2026-05-31 12:10:33 -0500

    version lite full

--- sw.js
+++ sw.js
@@ -1,22 +1,29 @@
-const CACHE = 'prestaya-v3';
-const ASSETS = ['./', './index.html'];
+const CACHE_NAME = 'prestaya-v2';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  'https://cdn.sheetjs.com/xlsx-0.20.2/package/dist/xlsx.full.min.js'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./')))
+    caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });
